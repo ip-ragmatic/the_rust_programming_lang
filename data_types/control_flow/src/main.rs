d5889521f0dd2eb27467e5693bda19ef3cc6eebb
ipragmatic <ipragmatic2r@gmail.com>
2024-07-03 22:54:03 -0400

    completed 3.5

--- data_types/control_flow/src/main.rs
+++ data_types/control_flow/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    test3();
+}
+
+fn test1() {
+    let arr = [2, 22, 42, 27, 69];
+    let mut idx = arr.len();
+
+    while idx > 0 {
+        idx -= 1;
+        println!("{}", arr[idx]);
+    }
+}
+
+fn test2() {
+    let arr = [2, 22, 42, 27, 69];
+    for &item in arr.iter().rev() {
+        println!("{}", item)
+    }
+}
+
+fn test3() {
+    for num in (1..4).rev() {
+        println!("{num}")
+    }
+    println!("LIFTOFF!!!")
 }
