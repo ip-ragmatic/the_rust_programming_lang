dbbafb3e30d58a8696523b88350ee904c7cc5444
ipragmatic <ipragmatic2r@gmail.com>
2024-10-24 00:56:06 -0400

    Completed Chapter-12
    Finished minigrep project. Slightly different than the books version point
    though because I used iterators, handled errors differently, and added
    slightly different functionality. Also handled precedence between env
    var and flag for ignore_case search

---
+++ Chapter-12/minigrep/README.md
@@ -0,0 +1,7 @@
+# Minigrep Project From Ch. 12 of "The Book"
+
+TODO implement color for line number and match in any matches made
+
+TODO figure out how to more intelligently detect flags, their arguments (if any), text query, and the file path
+
+TODO add more flags (e.g. regex pattern search)
---
+++ Chapter-12/minigrep/src/lib.rs
@@ -0,0 +1,105 @@
+use std::{env, error::Error, fs, process};
+
+pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    let file_text = fs::read_to_string(&config.path)?;
+
+    // ignore case or not check
+    let res = if config.ignore_case {
+        search_case_insensitive(&config.query, &file_text)
+    } else {
+        search(&config.query, &file_text)
+    };
+
+    res.iter()
+        .for_each(|(i, line)| println!("{}:{line}", i + 1)); // print each line in res
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<(usize, &'a str)> {
+    contents
+        .lines()
+        .enumerate()
+        .filter(|(_, line)| line.contains(query))
+        .collect()
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<(usize, &'a str)> {
+    let query = query.to_lowercase();
+
+    contents
+        .lines()
+        .enumerate()
+        .filter(|(_, line)| line.to_lowercase().contains(&query))
+        .collect()
+}
+
+pub struct Config {
+    pub query: String,
+    pub path: String,
+    pub ignore_case: bool,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("mg: minigrep requires @ least 1 pattern to exec a search");
+        }
+
+        let query = args[args.len() - 2].clone();
+        let path = args[args.len() - 1].clone();
+        let mut ignore_case = env::var("IGNORE_CASE").is_ok_and(|val| val == "1");
+
+        // iterate through potential flags slice and match cases
+        args[1..args.len() - 2]
+            .iter()
+            .for_each(|flag| match flag.as_str() {
+                "-U" => ignore_case = true,
+                flag => {
+                    eprintln!("mg: unrecognized flag {flag}");
+                    process::exit(1);
+                }
+            });
+
+        Ok(Config {
+            query,
+            path,
+            ignore_case,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec![(1, "safe, fast, productive.")],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec![(0, "Rust:"), (3, "Trust me.")],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
